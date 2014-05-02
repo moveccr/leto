42854b63263807b7c6134483523584df905efda3
Tetsuya Isaki <isaki@pastel-flower.jp>
2014-05-02 18:44:38 +0900

    ccr.h/ccr.cpp を廃止して perf2.cpp に内蔵する。 これはこれで独立した CCR についてのパフォーマンステストツール。 ccr.cpp は削除。ccr.h は同じ名前で使う予定のため放置してある。

--- leto060/perf2.cpp
+++ leto060/perf2.cpp
@@ -1,11 +1,124 @@
-#include <stdint.h>
+//
+// CCR のテスト
+
+// CCRをどう実装したらいいかテストしたい。
+
+// 1. mame みたいに独立したバイト変数として実装。
+// 2. XEiJ みたいに複合したバイト変数として実装。
+// 3. ビットフィールドを使う。
+// 5. アセンブラでの演算結果のフラグを使用する。
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/time.h>
 #include "stopwatch.h"
-#include "ccr.h"
 
 #define countof(x)	(sizeof(x) / sizeof(x[0]))
+
+#define CCR_X 0x0010
+#define CCR_N 0x0008
+#define CCR_Z 0x0004
+#define CCR_V 0x0002
+#define CCR_C 0x0001
+
+class CCR1
+{
+	// これが MPU だと思いねえ。
+
+ public:
+	CCR1()
+		: FlagNZ(0), FlagC(0), FlagV(0), FlagS(0), FlagX(0)
+	{
+	}
+
+	uint8_t get()
+	{
+		return ((FlagX & 0x80) ? CCR_X : 0)
+		     | ((FlagS & 0x80) ? CCR_N : 0)
+		     | (FlagNZ ? 0 : CCR_Z)
+		     | ((FlagV & 0x80) ? CCR_V : 0)
+		     | ((FlagC & 0x80) ? CCR_C : 0);
+	}
+
+	uint32_t FlagNZ;	// 値が == 0 なら Z=1
+	// 以下いずれも 0x80 のビットで判定する
+	uint8_t FlagC;
+	uint8_t FlagV;
+	uint8_t FlagS;	// N フラグ。Z を NZ としているので S に変えとく。
+	uint8_t FlagX;
+};
+
+
+class CCR2
+{
+	// これが MPU だと思いねえ。
+ public:
+	CCR2()
+		: CCR(0)
+	{
+	}
+
+	uint8_t get() { return CCR; }
+
+	uint8_t CCR;	// XNZVC
+};
+
+class CCR3
+{
+	// これが MPU だと思いねえ。
+ public:
+	CCR3()
+		: FlagZ(0), FlagC(0), FlagV(0), FlagN(0), FlagX(0)
+	{
+	}
+
+	bool FlagZ: 1;
+	bool FlagC: 1;
+	bool FlagV: 1;
+	bool FlagN: 1;
+	bool FlagX: 1;
+};
+
+class CCR5
+{
+	// x64 の FLAG レジスタ
+	static const int FLAG_CF = 0x0001;	// Carry
+	static const int FLAG_ZF = 0x0040;	// Zero
+	static const int FLAG_SF = 0x0080;	// Sign
+
+ public:
+	CCR5()
+		: FlagNZVC(0), X(0)
+	{
+	}
+
+	uint8_t get()
+	{
+		return (FlagX() ? CCR_X : 0)
+		     | (FlagN() ? CCR_N : 0)
+		     | (FlagZ() ? CCR_Z : 0)
+		     | (FlagV() ? CCR_V : 0)
+		     | (FlagC() ? CCR_C : 0);
+	}
+
+	// LAHF 命令でフラグは AH (AX の上位バイト) にロードされる。
+	bool FlagN() { return (FlagNZVC & (FLAG_SF << 8)); }
+	bool FlagZ() { return (FlagNZVC & (FLAG_ZF << 8)); }
+	bool FlagC() { return (FlagNZVC & (FLAG_CF << 8)); }
+
+	// Vフラグに相当する OV フラグは LAHF 命令では取り出せないので
+	//  SETO 命令を使って AX のビット 0 を立てている。
+	bool FlagV() { return (FlagNZVC & 0x0001); }
+
+	// XフラグのビットはCフラグのそれと同じ位置にしておく
+	bool FlagX() { return (X & (FLAG_CF << 8)); }
+
+	uint16_t FlagNZVC;	// N,Z,V,C
+	uint16_t X;
+};
+
+
 
 // CCR1
 inline uint8_t
